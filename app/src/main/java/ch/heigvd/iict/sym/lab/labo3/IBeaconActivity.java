84cac5bee18f771c1c8e3c2c1eaedd26fc3b1b31
Mario Tomic <mario-tomic@hotmail.com>
2021-12-19 00:14:26 +0100

    IBeacon ok, en principe

--- app/src/main/java/ch/heigvd/iict/sym/lab/labo3/IBeaconActivity.java
+++ app/src/main/java/ch/heigvd/iict/sym/lab/labo3/IBeaconActivity.java
@@ -1,14 +1,83 @@
 package ch.heigvd.iict.sym.lab.labo3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.Observer;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.Region;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+
 
 public class IBeaconActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
+
+    private ArrayList<Beacon> allBeacons;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ibeacon);
+
+        CheckPermissions();
+
+        listView = findViewById(R.id.beaconList);
+
+        allBeacons = new ArrayList<>();
+
+        IBeaconAdapter iba = new IBeaconAdapter(this, R.layout.beacon_display, allBeacons);
+        listView.setAdapter(iba);
+        BeaconManager bm = BeaconManager.getInstanceForApplication(this);
+        bm.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+        Region region = new Region("all-beacons-region", null, null, null);
+        bm.getRegionViewModel(region).getRangedBeacons().observe(this, monitoringObserver);
+        bm.startRangingBeacons(region);
+
+    }
+
+    Observer monitoringObserver = (Observer<Collection<Beacon>>) beacons -> {
+        allBeacons.clear();
+        allBeacons.addAll(beacons);
+        // https://stackoverflow.com/a/4198569
+        ((BaseAdapter) listView.getAdapter()).notifyDataSetChanged();
+    };
+
+
+
+    private void CheckPermissions(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                    PERMISSION_REQUEST_FINE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_FINE_LOCATION) {
+            if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
---
+++ app/src/main/java/ch/heigvd/iict/sym/lab/labo3/IBeaconAdapter.java
@@ -0,0 +1,50 @@
+package ch.heigvd.iict.sym.lab.labo3;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.altbeacon.beacon.Beacon;
+
+import java.util.ArrayList;
+
+//https://stackoverflow.com/questions/8166497/custom-adapter-for-list-view
+public class IBeaconAdapter extends ArrayAdapter<Beacon> {
+
+    private int resourceLayout;
+    private Context mContext;
+
+    public IBeaconAdapter(Context context, int resource, ArrayList<Beacon> items) {
+        super(context, resource, items);
+        this.resourceLayout = resource;
+        this.mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(mContext);
+            view = vi.inflate(resourceLayout, null);
+        }
+        Beacon b = getItem(position);
+
+        TextView bluetoothAddress = view.findViewById(R.id.bluetoothAddress);
+        TextView uuid = view.findViewById(R.id.uuid);
+        TextView majmin = view.findViewById(R.id.majmin);
+        TextView rssi = view.findViewById(R.id.rssi);
+
+        bluetoothAddress.setText(b.getBluetoothAddress());
+        uuid.setText("uuid : " + b.getId1().toString());
+        majmin.setText("Majeur : " + b.getId2().toString() + " Mineur : " + b.getId3().toString());
+        rssi.setText("RSSI : " + b.getRssi());
+
+        return view;
+    }
+}
